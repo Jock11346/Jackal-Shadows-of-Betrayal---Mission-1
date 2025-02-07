aeca21f5d604b74205b958d25927b7f7225e33c3
Jock11346 <judequigley26@gmail.com>
2025-02-07 21:33:36 +0000

    Update Mission1Scene.js

--- js/scenes/Mission1Scene.js
+++ js/scenes/Mission1Scene.js
@@ -4,15 +4,10 @@ export default class Mission1Scene extends Phaser.Scene {
     }
 
     preload() {
-        // Load images
-        this.load.image('facilityBackground', 'assets/images/facility.png');
-        this.load.image('informant', 'assets/images/informant.png');
-        this.load.image('guard', 'assets/images/guard.png');
-
-        // Load audio
-        this.load.audio('missionMusic', 'assets/audio/Assassin.mp3');
-        this.load.audio('footsteps', 'assets/audio/footsteps.mp3');
-        this.load.audio('silencedGunshot', 'assets/audio/silencedGunshot.mp3');
+        // Load assets for Mission 1
+        this.load.image('facilityBackground', 'assets/images/facility.jpg');
+        this.load.image('informant', 'assets/images/informant.jpg');
+        this.load.image('guard', 'assets/images/guard.jpg');
     }
 
     create() {
@@ -24,22 +19,16 @@ export default class Mission1Scene extends Phaser.Scene {
         this.add.image(200, 400, 'informant').setScale(0.5);
         this.add.image(600, 400, 'guard').setScale(0.5);
 
-        // Play background music
-        const missionMusic = this.sound.add('missionMusic', { loop: true });
-        missionMusic.play();
-
         // Display mission objective
         this.add.text(16, 16, "Mission Objective: Decide the informant's fate", 
             { fontSize: '20px', fill: '#fff' });
 
         // Create buttons for player choices
         createButton(this, 'Spare Informant', 100, 400, '#00ff00', () => {
-            this.sound.play('footsteps'); // Play footsteps sound
             this.scene.start('StealthScene', { action: 'spare' });
         });
 
         createButton(this, 'Kill Informant', 100, 450, '#ff0000', () => {
-            this.sound.play('silencedGunshot'); // Play silenced gunshot sound
             this.scene.start('StealthScene', { action: 'kill' });
         });
     }
