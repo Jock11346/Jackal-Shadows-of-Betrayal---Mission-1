f3ffec717859c40cf04555fbd9e4342121f31f65
Jock11346 <judequigley26@gmail.com>
2025-02-08 11:14:21 +0000

    Update StealthScene.js

--- js/scenes/StealthScene.js
+++ js/scenes/StealthScene.js
@@ -1,36 +1,22 @@
-import { createButton } from '../js/ButtonUtils.js';
-
 export default class StealthScene extends Phaser.Scene {
     constructor() {
         super({ key: 'StealthScene' });
     }
 
     preload() {
-        // Load images (all paths are lowercase and match GitHub)
-        this.load.image('container', 'assets/images/container.jpg');
-        this.load.image('jackal', 'assets/images/jackal.jpg');
+        this.load.image('stealth_bg', 'assets/images/environments/stealth_bg.webp');
     }
 
-    create(data) {
-        // Add background image (uses facilityBackground loaded in Mission1Scene)
-        this.add.image(this.cameras.main.centerX, this.cameras.main.centerY, 'facilityBackground')
-            .setOrigin(0.5, 0.5);
+    create() {
+        // Stealth mechanics setup
+        this.add.image(0, 0, 'stealth_bg').setOrigin(0);
+        this.player = this.physics.add.sprite(200, 500, 'raven');
 
-        // Add player sprite
-        const player = this.add.image(100, 400, 'jackal').setScale(0.5);
+        // Visibility detection
+        this.lightMask = this.make.graphics().fillStyle(0x000000, 0.7).fillRect(0, 0, 1280, 720);
+    }
 
-        // Handle player choice from Mission1Scene
-        if (data.action === 'spare') {
-            this.add.text(16, 50, 'You chose to spare the informant.', 
-                { fontSize: '20px', fill: '#00ff00' });
-        } else if (data.action === 'kill') {
-            this.add.text(16, 50, 'You chose to kill the informant.', 
-                { fontSize: '20px', fill: '#ff0000' });
-        }
-
-        // Add a button to return to the main menu
-        createButton(this, 'Return to Main Menu', 300, 500, '#ffffff', () => {
-            this.scene.start('MainMenuScene');
-        });
+    update() {
+        // Stealth movement logic
     }
 }
