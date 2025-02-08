a1d2a42e2053a2ba7b9e7e2a9b7e04b4f3a1a120
Jock11346 <judequigley26@gmail.com>
2025-02-08 18:36:25 +0000

    Update Game.js

--- js/Game.js
+++ js/Game.js
@@ -1,25 +1,12 @@
-import PreloadScene from './scenes/PreloadScene.js';
 import MainMenuScene from './scenes/MainMenuScene.js';
 import Mission1Scene from './scenes/Mission1Scene.js';
 import StealthScene from './scenes/StealthScene.js';
 
 const config = {
     type: Phaser.AUTO,
-    width: 1280,
-    height: 720,
-    parent: 'game-container',
-    physics: {
-        default: 'arcade',
-        arcade: { debug: false }
-    },
-    scene: [PreloadScene, MainMenuScene, Mission1Scene, StealthScene],
-    scale: {
-        mode: Phaser.Scale.FIT,
-        autoCenter: Phaser.Scale.CENTER_BOTH
-    }
+    width: 800,
+    height: 600,
+    scene: [MainMenuScene, Mission1Scene, StealthScene]
 };
 
 const game = new Phaser.Game(config);
-
-// Signal that the game has loaded (call this in PreloadScene when ready)
-window.gameLoaded = true;
