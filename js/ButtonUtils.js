108fb26fc2a4e27d3c3c4503732f37b67d92db41
Jock11346 <judequigley26@gmail.com>
2025-02-09 13:51:08 +0000

    Update ButtonUtils.js

--- js/ButtonUtils.js
+++ js/ButtonUtils.js
@@ -1,10 +1,16 @@
-export function createButton(scene, text, x, y, color, callback, hoverColor = '#ff0', fontSize = '24px') {
-    const button = scene.add.text(x, y, text, { fontSize, fill: color })
-        .setOrigin(0.5, 0.5)
-        .setInteractive()
-        .on('pointerdown', callback)
-        .on('pointerover', () => button.setStyle({ fill: hoverColor }))
-        .on('pointerout', () => button.setStyle({ fill: color }));
+export function createButton(scene, text, x, y, color, callback) {
+  const button = scene.add.text(x, y, text, {
+    fontSize: '24px',
+    fill: '#000',
+    backgroundColor: color,
+    padding: { x: 10, y: 5 },
+    align: 'center'
+  }).setOrigin(0.5)
+    .setInteractive({ useHandCursor: true })
+    .on('pointerup', callback)
+    .on('pointerover', () => button.setStyle({ fill: '#ff0' }))
+    .on('pointerout', () => button.setStyle({ fill: '#000' }));
 
-    return button;
+  return button;
 }
+
